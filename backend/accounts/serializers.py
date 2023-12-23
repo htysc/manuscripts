aae68766b92b21133711bf5b11ca299b3f49f4e8
Subhasis Chakraborti <subhasis.chakraborti@mail.utoronto.ca>
2023-12-23 13:47:03 -0500

    Fix user serializer

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth.hashers import make_password
 
 class UserSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, style={'input_type': 'password'})
+    password = serializers.CharField(write_only=True, required=False, style={'input_type': 'password'})
 
     class Meta:
         model = User
