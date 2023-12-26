2668987aa7b351caaec15cddceb5363ee8696fbb
Subhasis Chakraborti <subhasis.chakraborti@mail.utoronto.ca>
2023-12-26 00:01:36 -0500

    Backend fix

--- backend/manuscripts/wsgi.py
+++ backend/manuscripts/wsgi.py
@@ -6,6 +6,9 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
+import sys
+sys.path.append('/var/www/manuscripts/backend')
+sys.path.append('/var/www/manuscripts/backend/manuscripts')
 
 import os
 
